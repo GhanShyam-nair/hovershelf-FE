d7075408a15164fc68d3ec478c2bd1646fa034b9
Ghanshyam S Nair <91174790+GhanShyam-nair@users.noreply.github.com>
2025-05-09 19:26:08 +0530

    workbar finished

--- hovershelf-ui/src/app/Pages/work/work.component.ts
+++ hovershelf-ui/src/app/Pages/work/work.component.ts
@@ -1,11 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { animate, state, style, transition, trigger } from '@angular/animations';
+
+import { CommonModule } from '@angular/common';
+
+// import { RouterModule } from '@angular/router';
+// import { FooterComponent } from '../';
+
 
 @Component({
   selector: 'app-work',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './work.component.html',
-  styleUrl: './work.component.scss'
+  styleUrls: ['./work.component.scss'],
+  animations: [
+    trigger('slide', [
+      state('void', style({
+        transform: 'translateX(-100%)'
+      })),
+      transition(':enter', [
+        animate('1s ease-out', style({
+          transform: 'translateX(0)'
+        }))
+      ]),
+      transition(':leave', [
+        animate('1s ease-out', style({
+          transform: 'translateX(100%)'
+        }))
+      ])
+    ])
+  ]
 })
-export class WorkComponent {
+export class WorkComponent implements OnInit {
+  workItems = [
+    { title: 'Cheddar ai - mcp ', imageUrl: '../../../assets/Cheddar.png' },
+    { title: 'Cheddar AI', imageUrl: '../../../assets/Dafmail.png' },
+    { title: 'ASPotify', imageUrl: '../../../assets/Aspotify.png' },
+    { title: 'ASPotify', imageUrl: '../../../assets/Mangalyavedi.png' },
+    { title: 'Mangalyavedi', imageUrl: '../../../assets/Sundress.png' },
+    { title: 'Mangalyavedi', imageUrl: '../../../assets/Hovershelfshop.png' },
+    { title: 'GHEEmail', imageUrl: '../../../assets/Flop.png' },
+    { title: 'DaftMail', imageUrl: '../../../assets/Ape.png' }
+  ];
+  
 
+  constructor() { }
+
+  ngOnInit(): void {
+  }
 }
